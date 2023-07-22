9f5ae4715b2321b87449c01031c26b0185ce124f
leo.forster <forster@krebsregister-rlp.de>
2023-07-22 11:56:19 +0200

    _

--- src/DependencyCrawler.QueryClient/DependencyCrawler.cs
+++ src/DependencyCrawler.QueryClient/DependencyCrawler.cs
@@ -1,4 +1,5 @@
 using DependencyCrawler.Contracts.Interfaces;
+using DependencyCrawler.Framework.Extensions;
 
 namespace DependencyCrawler.QueryClient;
 
@@ -6,17 +7,25 @@ public class DependencyCrawler : IDependencyCrawler
 {
 	private readonly IProjectLoader _projectLoader;
 	private readonly IProjectProvider _projectProvider;
+	private readonly IProjectQueries _projectQueries;
 
-	public DependencyCrawler(IProjectProvider projectProvider, IProjectLoader projectLoader)
+	public DependencyCrawler(IProjectProvider projectProvider, IProjectLoader projectLoader,
+		IProjectQueries projectQueries)
 	{
 		_projectProvider = projectProvider;
 		_projectLoader = projectLoader;
+		_projectQueries = projectQueries;
 	}
 
 	public void Run()
 	{
 		_projectLoader.LoadAllProjects();
-		//var project = _projectLoader.LoadProjectByName("");
+
 		var allProjects = _projectProvider.AllProjects;
+		var dependingProjects = _projectProvider.AllProjects.Where(x => x.Value.DependsOn(""));
+		var allDependencies = _projectProvider.AllProjects[""].GetAllDependenciesRecursive();
+		var usages = _projectQueries.GetUsagesForProject("");
+		var internalTopLevelProjects = _projectQueries.GetInternalTopLevelProjects();
+		var subLevelProjects = _projectQueries.GetSubLevelProjects();
 	}
 }
