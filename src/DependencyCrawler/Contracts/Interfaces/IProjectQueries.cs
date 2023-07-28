11c2675c29bb7ea36154a5d9390d44c8224eee2d
leo.forster <forster@krebsregister-rlp.de>
2023-07-28 14:07:48 +0200

    _

--- src/DependencyCrawler/Contracts/Interfaces/IProjectQueries.cs
+++ src/DependencyCrawler/Contracts/Interfaces/IProjectQueries.cs
@@ -4,8 +4,7 @@ namespace DependencyCrawler.Contracts.Interfaces;
 
 public interface IProjectQueries
 {
-	IEnumerable<IProject> GetInternalTopLevelProjects();
-	IEnumerable<IProject> GetSubLevelProjects();
-	IEnumerable<IProject> GetUsagesForProject(string projectName);
-	IEnumerable<IProject> GetTopLevelProjects();
+    IEnumerable<IProject> GetInternalTopLevelProjects();
+    IEnumerable<IProject> GetSubLevelProjects();
+    IEnumerable<IProject> GetTopLevelProjects();
 }
--- src/DependencyCrawler/Implementations/Repositories/ProjectQueries.cs
+++ src/DependencyCrawler/Implementations/Repositories/ProjectQueries.cs
@@ -6,42 +6,34 @@ namespace DependencyCrawler.Implementations.Repositories;
 
 public class ProjectQueries : IProjectQueries
 {
-	private readonly IProjectProvider _projectProvider;
+    private readonly IProjectProvider _projectProvider;
 
-	public ProjectQueries(IProjectProvider projectProvider)
-	{
-		_projectProvider = projectProvider;
-	}
+    public ProjectQueries(IProjectProvider projectProvider)
+    {
+        _projectProvider = projectProvider;
+    }
 
-	public IEnumerable<IProject> GetInternalTopLevelProjects()
-	{
-		var toplevelProjects = _projectProvider.InternalProjects
-			.Where(x => !x.Dependencies.Any() ||
-			            x.Dependencies.Values.All(y => y.Using.ProjectType == ProjectType.External));
+    public IEnumerable<IProject> GetInternalTopLevelProjects()
+    {
+        var toplevelProjects = _projectProvider.InternalProjects
+            .Where(x => !x.Dependencies.Any() ||
+                        x.Dependencies.Values.All(y => y.Using.ProjectType == ProjectType.External));
 
-		return toplevelProjects;
-	}
+        return toplevelProjects;
+    }
 
-	public IEnumerable<IProject> GetSubLevelProjects()
-	{
-		var projects = _projectProvider.AllProjects;
-		return projects.Where(x => projects.All(y => y.Value.Dependencies.All(z => z.Value.Using != x.Value)))
-			.Select(x => x.Value);
-	}
+    public IEnumerable<IProject> GetSubLevelProjects()
+    {
+        var projects = _projectProvider.AllProjects;
+        return projects.Where(x => projects.All(y => y.Value.Dependencies.All(z => z.Value.Using != x.Value)))
+            .Select(x => x.Value);
+    }
 
-	public IEnumerable<IProject> GetUsagesForProject(string projectName)
-	{
-		var projects = _projectProvider.AllProjects;
+    public IEnumerable<IProject> GetTopLevelProjects()
+    {
+        var toplevelProjects = _projectProvider.AllProjects.Values
+            .Where(x => !x.Dependencies.Any());
 
-		return projects.Where(x => x.Value.Dependencies.Any(y => y.Value.Using.Name == projectName))
-			.Select(x => x.Value);
-	}
-
-	public IEnumerable<IProject> GetTopLevelProjects()
-	{
-		var toplevelProjects = _projectProvider.AllProjects.Values
-			.Where(x => !x.Dependencies.Any());
-
-		return toplevelProjects;
-	}
+        return toplevelProjects;
+    }
 }
