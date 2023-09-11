aee8185b8858b8896bf4006174374d307cef8bdf
leo.forster <LeoForster@t-online.de>
2023-09-11 14:41:50 +0200

    _

--- src/DependencyCrawler/Implementations/Repositories/Provider/ProjectFileProvider.cs
+++ src/DependencyCrawler/Implementations/Repositories/Provider/ProjectFileProvider.cs
@@ -7,32 +7,40 @@ namespace DependencyCrawler.Implementations.Repositories.Provider;
 
 internal class ProjectFileProvider : IProjectFileProvider
 {
-    private const string Extension = "*.csproj";
-    private readonly IConfiguration _configuration;
+	private const string Extension = "*.csproj";
+	private readonly IConfiguration _configuration;
+	private IEnumerable<string>? _projectFiles;
 
-    public ProjectFileProvider(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
+	public ProjectFileProvider(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
 
-    public IEnumerable<string> GetProjectFiles()
-    {
-        var path = _configuration[ConfigurationKeys.RootDirectory.ToString()];
-        return Directory.GetFiles(path!, Extension, SearchOption.AllDirectories).ToList();
-    }
+	public IEnumerable<string> GetProjectFiles()
+	{
+		if (_projectFiles is not null)
+		{
+			return _projectFiles;
+		}
 
-    public string? GetProjectFile(string projectName)
-    {
-        return GetProjectFiles().FirstOrDefault(x => x.GetProjectName() == projectName);
-    }
+		var path = _configuration[ConfigurationKeys.RootDirectory.ToString()];
+		_projectFiles = Directory.GetFiles(path!, Extension, SearchOption.AllDirectories);
 
-    public string? GetProjectDirectory(string projectName)
-    {
-        return Path.GetDirectoryName(GetProjectFile(projectName));
-    }
+		return _projectFiles;
+	}
 
-    public bool GetIsInternal(string projectName)
-    {
-        return GetProjectFiles().Any(x => x.GetProjectName() == projectName);
-    }
+	public string? GetProjectFile(string projectName)
+	{
+		return GetProjectFiles().FirstOrDefault(x => x.GetProjectName() == projectName);
+	}
+
+	public string? GetProjectDirectory(string projectName)
+	{
+		return Path.GetDirectoryName(GetProjectFile(projectName));
+	}
+
+	public bool GetIsInternal(string projectName)
+	{
+		return GetProjectFiles().Any(x => x.GetProjectName() == projectName);
+	}
 }
