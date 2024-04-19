9eb8fa394965316add70a021d14a22539da05854
leo.forster <LeoForster@t-online.de>
2024-04-19 08:47:43 +0200

    _

--- src/DependencyCrawler.DataCore.DataAccess/IDependencyCrawlerCore.cs
+++ src/DependencyCrawler.DataCore.DataAccess/IDependencyCrawlerCore.cs
@@ -6,6 +6,4 @@ public interface IDependencyCrawlerCore
 {
 	ConcurrentDictionary<string, Module> Modules { get; }
 	Guid Id { get; }
-	IDependencyCrawlerDataAccess DependencyCrawlerDataAccess { get; init; }
-	bool IsActive { get; }
 }
